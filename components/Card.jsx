167e9655ff7da9934352a85b49c9a775d53f75db
Siva Pulagam <vallururohit@gmail.com>
2024-08-30 12:31:08 +0530

    Card component can access dynamic data

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,17 +1,19 @@
 import { View, Text, Image } from 'react-native'
 import React from 'react'
+import useStore from '../store/store'
 
 const Card = () => {
+  const {vehicleName, engineCC, vehicleType, imageUri} = useStore();
   return (
     <View className="justify-center items-center mt-10">
           <View className="bg-gray-200 h-52 w-80 rounded-xl bg-cover">
-            <Image source={require('../assets/road.jpg')} className='h-40 w-80'/>
+            <Image source={{ uri: imageUri }} className='h-40 w-80'/>
           <View className="flex flex-row gap-28 justify-center items-center">
             <View>
-              <Text className="text-sky-900 font-semibold text-lg">Yamaha R15</Text>
-              <Text className="text-gray-500 font-semibold">2 wheeler</Text>
+              <Text className="text-sky-900 font-semibold text-lg">{vehicleName}</Text>
+              <Text className="text-gray-500 font-semibold">{vehicleType}</Text>
             </View>
-            <Text className="text-sky-900 font-semibold text-base">155 CC</Text>
+            <Text className="text-sky-900 font-semibold text-base">{engineCC}</Text>
           </View>
           </View>
         </View>
